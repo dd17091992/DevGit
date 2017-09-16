8424cb9cffbdd7e0dbfba2c29b273ac82ccf52d5
dd17091992 <dubaled1514@gmail.com>
2017-09-16 18:19:40 +0530

    Dev Git 16/09

---
+++ src/test/java/com/pack/DevGit/NewTest.java
@@ -0,0 +1,38 @@
+package com.pack.DevGit;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class NewTest
+{
+  WebDriver driver;
+  @Test
+  public void f() 
+  {
+	  System.out.println("GIT PROJECT UPDATED");
+	   
+  }
+  @BeforeClass
+  public void beforeClass() 
+  {
+	  System.setProperty("webdriver.chrome.driver", "Resource/chromedriver.exe");
+	  driver = new ChromeDriver();
+	  driver.get("https://dev.tracko.co.in/");
+	  
+	  driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+	  driver.manage().window().maximize(); 
+  }
+
+  @AfterClass
+  public void afterClass() 
+  {
+	 driver.quit(); 
+  }
+
+}
